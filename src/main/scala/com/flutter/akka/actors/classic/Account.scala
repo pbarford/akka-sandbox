dbf35cf49603cfe88d98788c86e5088a54aa5180
Paul Barford <paul.barford@ppb.com>
2023-09-22 15:30:22 +0100

    updates

--- src/main/scala/com/flutter/akka/actors/classic/Account.scala
+++ src/main/scala/com/flutter/akka/actors/classic/Account.scala
@@ -2,16 +2,21 @@ package com.flutter.akka.actors.classic
 
 import akka.actor.{ActorLogging, ActorRef, Props}
 import akka.cluster.sharding.ShardRegion
-import akka.persistence.PersistentActor
+import akka.cluster.sharding.ShardRegion.Passivate
 import akka.persistence.typed.state.RecoveryCompleted
-import com.flutter.akka.actors.classic.Account.{AccountBalance, AccountCommand, AccountCredited, AccountDebited, AccountEvent, AccountState, Deposit, GetBalance, Withdraw, WithdrawalDeclined}
+import akka.persistence._
+import com.flutter.akka.actors.classic.Account._
 
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
 object Account {
 
   sealed trait AccountCommand {
     def accountNo:String
   }
 
+  case object Die
+  case object Stop
   case class Deposit(accountNo: String, amount: Double) extends AccountCommand
   case class Withdraw(accountNo: String, amount: Double) extends AccountCommand
   case class GetBalance(accountNo: String) extends AccountCommand
@@ -26,11 +31,11 @@ object Account {
   case class WithdrawalDeclined(accountNo: String, timestamp: Long, amount: Double) extends AccountEvent
   case class AccountBalance(accountNo: String, timestamp: Long, totalBalance: Double, transactions:List[AccountEvent]) extends AccountEvent
 
-  case class AccountState(accountNo: String, balance: Double = 0.0, transactions: List[AccountEvent] = List.empty) {
+  case class AccountState(accountNo: String, seqNo:Long = 0, balance: Double = 0.0, transactions: List[AccountEvent] = List.empty) {
     def apply: AccountEvent => AccountState = {
-      case credit: AccountCredited => copy(balance = balance + credit.amount, transactions = credit :: transactions)
-      case debit: AccountDebited => copy(balance= balance - debit.amount, transactions = debit :: transactions)
-      case declined: WithdrawalDeclined => copy(transactions = declined :: transactions)
+      case credit: AccountCredited => copy(seqNo = seqNo + 1, balance = balance + credit.amount, transactions = credit :: transactions)
+      case debit: AccountDebited => copy(seqNo = seqNo + 1, balance= balance - debit.amount, transactions = debit :: transactions)
+      case declined: WithdrawalDeclined => copy(seqNo = seqNo + 1, transactions = declined :: transactions)
       case _ => this
     }
   }
@@ -66,6 +71,10 @@ object Account {
 
 class Account() extends PersistentActor with ActorLogging {
 
+  private implicit val executionContext: ExecutionContextExecutor = context.system.dispatcher
+
+  context.system.scheduler.scheduleOnce(5.seconds, self, Die)
+
   private def accountNo: String = self.path.name
 
   private var state: AccountState = AccountState(accountNo)
@@ -79,10 +88,14 @@ class Account() extends PersistentActor with ActorLogging {
 
   private def applyEventToState: AccountEvent => AccountEvent = { ev =>
     state = state.apply(ev)
+    if(!recoveryRunning && state.seqNo % 3 == 0) saveSnapshot(state)
     ev
   }
 
   override def receiveRecover: Receive = {
+    case SnapshotOffer(metadata, snapshot:AccountState) =>
+      log.info(s"Snapshot received HWM [${metadata.sequenceNr}]")
+      state = snapshot
     case ev: AccountEvent => applyEventToState(ev)
     case _: RecoveryCompleted => ()
   }
@@ -99,6 +112,22 @@ class Account() extends PersistentActor with ActorLogging {
     case cmd: AccountCommand =>
       log.info(s"Actor::Account --> AccountCommand received [$cmd]")
       applyCommand.andThen(persistAndReply(sender()))(cmd)
+
+    case SaveSnapshotSuccess(metadata) =>
+      log.info(s"Snapshot saved seqNo [${metadata.sequenceNr}]")
+      deleteMessages(metadata.sequenceNr)
+      deleteSnapshots(SnapshotSelectionCriteria.create(metadata.sequenceNr - 1, System.currentTimeMillis()))
+
+    case DeleteSnapshotsSuccess(criteria) => log.info(s"DeleteSnapshotsSuccess to seqNo [${criteria.maxSequenceNr}]")
+    case DeleteMessagesSuccess(toSeqNo) => log.info(s"DeleteMessagesSuccess to seqNo [$toSeqNo]")
+
+    case Die =>
+      log.info(s"Die received, committing hare kari")
+      context.parent ! Passivate(stopMessage = Stop)
+
+    case Stop =>
+      log.info(s"Stop received")
+      context.stop(self)
   }
   
   override def persistenceId: String = s"classic-acc-$accountNo"
